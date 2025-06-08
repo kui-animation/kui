3fd04507edd49147f31d1da77c50d46e63d4a518
andrej <andrej.ciperle@gmail.com>
2025-06-08 19:35:41 +0200

    refectoring

--- src/main.asm
+++ src/main.asm
@@ -18,14 +18,18 @@
 %define STDOUT 1
 
 
-section .data
-    mov_cur db 0x1b, "[1A"
+section .data                           ; defining constants
     sleep_time dq 0, 250000000
+    mov_cur db 0x1b, "[1A"
+    lode db "do you what to lode a file?", 0xA
+    lode_len equ $ - lode
+    what_file db "what is the file name?", 0xA
+    what_file_len equ $ - what_file
 
-section .bss
+section .bss                            ; defining variables
     file_prt resb 8
     mes resb LINE_LEN
-    new_file resb 2
+    lode_file resb 2
     buf resb RES+1
     file resb 16
 
@@ -34,29 +38,43 @@ global _start
 
 _start:
 
-    mov rax, SYS_READ
-    mov rdi, STDIN
-    mov rsi, new_file
-    mov rdx, 2
+    mov rax, SYS_WRITE
+    mov rdi, STDOUT
+    mov rsi, lode
+    mov rdx, lode_len
     syscall
 
-    cmp byte[new_file], 'y'
+                                        ; get input 
+    mov rax, SYS_READ
+    mov rdi, STDIN
+    mov rsi, lode_file
+    mov rdx, 2
+    syscall
+                                        ; check if file exist
+
+    cmp byte[lode_file], 'y'
     je get_file_name
 
+    cmp byte[lode_file], 'n'
+    jne skip_removeing_file
+
+                                        ; set file name
     mov byte[file], 0x41
     mov byte[file+1], 0x0
 
-                            ; open a file
+                                        ; open a file
     mov rax, SYS_OPEN
     mov rdi, file
-    mov rsi, WRITE_F | CREAT_F         ; creat now file in write mode
+    mov rsi, WRITE_F | CREAT_F          ; creat now file in write mode
     mov rdx, FILE_NUM
     syscall
-    mov [file_prt], rax     ; set file pointer to the fd
+    mov [file_prt], rax                 ; set file pointer to the fd
 
-loop:
 
-                            ; read input in mes
+
+read_comand:
+
+                                        ; read input in mes
     mov rax, SYS_READ
     mov rdi, STDIN
     mov rsi, mes
@@ -64,31 +82,37 @@ loop:
     syscall
 
     cmp rax, LINE_LEN
-    jl start_exe
+    jl start_executing_program
 
-                            ; comper if mes is end
+                                        ; comper if mes is end
     cmp byte[mes], 'e'
-    jne print_loop
+    jne write_comand
     cmp byte[mes+1], 'n'
-    jne print_loop
+    jne write_comand
     cmp byte[mes+2], 'd'
-    jne print_loop
+    jne write_comand
     cmp byte[mes+3], 0xA
-    je start_exe            ; if is end go start_exe
+    je start_executing_program          ; if is end go start_exe
 
-print_loop:
-   
-                            ; write to the tmp file 
+write_comand:
+
+                                        ; write to the tmp file 
     mov rdi, [file_prt]
     mov rdx, rax
     mov rax, SYS_WRITE
     mov rsi, mes
     syscall
 
-    jmp loop                ; go to start of loop
+    jmp read_comand                     ; go to start of loop
 
 
 get_file_name:
+    mov rax, SYS_WRITE
+    mov rdi, STDOUT
+    mov rsi, what_file
+    mov rdx, what_file_len
+    syscall
+
     mov rax, SYS_READ
     mov rdi, STDIN
     mov rsi, file
@@ -98,38 +122,38 @@ get_file_name:
     mov byte[file+rax-1], 0x0
     jmp open_file
 
-start_exe:
-                            ; close file
+start_executing_program:
+                                        ; close file
     mov rax, SYS_CLOSE
     mov rdi, [file_prt]
     syscall 
     
 open_file:
-                            ; open it in read mode
+                                        ; open it in read mode
     mov rax, SYS_OPEN
     mov rdi, file
     mov rsi, READ_F
     mov rdx, FILE_NUM
     syscall
 
-    mov [file_prt], rax     ; set file pointer to fd
+    mov [file_prt], rax                 ; set file pointer to fd
 
-exe_program:
-                            ; read char in file
+executing_program:
+                                        ; read char in file
     mov rax, SYS_READ
     mov rdi, [file_prt]
     mov rsi, mes
     mov rdx, LINE_LEN
     syscall
 
-                            ; if end off file go to end_program
+                                        ; if end off file go to end_program
     cmp rax, LINE_LEN
     jl end_program
 
     cmp byte[mes], 'v'
     jne .next_str1
     cmp byte[mes+1], 'o'
-    je voxel
+    je VOXEL
 
 .next_str1:
     cmp byte[mes], 's'
@@ -139,7 +163,7 @@ exe_program:
     cmp byte[mes+2], 'u'
     jne .next_str2
     cmp byte[mes+3], 'f'
-    je sbuf
+    je SBUF
 
 .next_str2:
 
@@ -153,7 +177,7 @@ exe_program:
     cmp byte[mes+3], 'e'
     jne .next_str3
     cmp byte[mes+4], 'p'
-    je sleep
+    je SLEEP
 
 .next_str3:
     cmp byte[mes], 'p'
@@ -168,40 +192,49 @@ exe_program:
     je print
 
 .next_str4:
-    jmp exe_program
+    jmp executing_program
 
 print:
     
-                            ; write char to the terminal
+                                        ; write char to the terminal
     mov rax, SYS_WRITE
     mov rdi, STDOUT
     mov rsi, buf
     mov rdx, RES
     syscall
 
-    call mov_up
+    call MOVEUP
 
-    jmp exe_program         ; go to exe_program
+    jmp executing_program
 
 
 end_program:
 
-                            ; close file
+                                        ; close file 
     mov rax, SYS_CLOSE
     mov rdi, [file_prt]
     syscall
 
-                            ; remove file
+    cmp byte[lode_file], 'y'
+    je skip_removeing_file
+                                        ; remove file
     mov rax, SYS_RMFILE
     mov rdi, file
     syscall
-    
-                            ; end program 
+
+skip_removeing_file:
+                                        ; end program 
     mov rax, SYS_EXIT
     mov rdi, 0
     syscall
 
-sbuf:
+
+;-----------;
+; FUNCTIONS ;
+;-----------;
+
+
+SBUF:
     mov al, byte[mes+4]
     mov rbx, 0
 .loop1:
@@ -212,11 +245,22 @@ sbuf:
     cmp rbx, RES
     jne .loop1
 
-    call fix_buf
+    call FIXBUF
 
-    jmp exe_program
+    jmp executing_program
 
-voxel:
+
+SLEEP:
+
+    mov rax, SYS_SLEEP
+    mov rdi, sleep_time
+    mov rsi, 0
+    syscall
+
+    jmp executing_program
+
+
+VOXEL:
 
     movzx eax, byte[mes+3]
     movzx ebx, byte[mes+2]
@@ -230,33 +274,10 @@ voxel:
     mov al, byte[mes+4]
     mov byte[buf+ecx], al
 
-    jmp exe_program
+    jmp executing_program
 
-sleep:
 
-    mov rax, SYS_SLEEP
-    mov rdi, sleep_time
-    mov rsi, 0
-    syscall
-
-    jmp exe_program
-
-fix_buf:
-    mov rbx, 0
-
-.loop1:
-    mov rax, rbx
-    imul rax, W_RES
-    mov byte[buf+rax], 0xA
-
-    inc rbx
-    cmp rbx, H_RES+1
-    jne .loop1
-
-    mov byte[buf+RES], 0x0
-    ret
-
-mov_up:
+MOVEUP:
     mov rbx, 0
 .loop1:
     mov rax, SYS_WRITE
@@ -269,4 +290,20 @@ mov_up:
     cmp rbx, H_RES+1
     jne .loop1
 
+    ret
+
+
+FIXBUF:
+    mov rbx, 0
+
+.loop1:
+    mov rax, rbx
+    imul rax, W_RES
+    mov byte[buf+rax], 0xA
+
+    inc rbx
+    cmp rbx, H_RES+1
+    jne .loop1
+
+    mov byte[buf+RES], 0x0
     ret
